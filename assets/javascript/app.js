afa16b34cf5f8bcc7ff8e80c4e30a912012ec11c
Mary Hoag <marymcevoyhoag@gmail.com>
2016-04-30 22:07:13 -0400

    first functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,34 @@
 //
+var app = {
+	var counter = 5;
+	var index;
+
+	questions: [
+	"What's with the pineapples? It's an inside joke.",
+	"The actor who plays Lassie once played a psychic.",
+	"Every actor from The Breakfast Club has appeared on Psych.",
+	"Shawn always chews Wrigley's doublemint gum.",
+	"In season 5, Gus teaches Lassiter to tap dance.",
+	"Since Shawn will never go fishing, Shawn's father takes Lassiter.",
+	"In one epsidoe, Juliette waits all day in a train station for an ex boyfriend.", 
+	"Shawn and Gus give Gus' car a nickname -- The Blackberry!",
+	"In one epsidoe, Shawn gets a part on a Bollywood movie.",
+	"Shawn's father is named Harry."
+	],
+
+	answers: [
+	true, true, false, false, true, true, true, false, false, false
+	],
+
+
+	//delivers answer after delay
+	countdown: function() {
+		setTimeout($(".gameboard")).html(answer[index], 6000);
+	}
+
+	//delivers next qustion after delay
+	newQuestion: function() {
+		setTimeout($".gameboard").html(question[index], 4000);
+	}
+
+	
